fd5e9fb605dfddec361d1e69295f32d94cb7a9d8
Karla <klopezm161@ulacit.ed.cr>
2020-08-05 20:42:27 -0600

    arreglando proyecto

--- EFServicioWeb/EFServicioWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace EFServicioWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- EFServicioWeb/EFServicioWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFServicioWeb.WebApiApplication" Language="C#" %>
