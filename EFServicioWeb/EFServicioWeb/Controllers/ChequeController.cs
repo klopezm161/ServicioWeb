cdac2ffca4cb77c5a680ab61ea36a74c449dcf5d
Karla <klopezm161@ulacit.ed.cr>
2020-08-07 16:10:26 -0600

    actualizando mas paquetes de NUGET

---
+++ EFServicioWeb/EFServicioWeb/Controllers/ChequeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Web.Http;
+using BLL;
+
+namespace EFServicioWeb.Controllers
+{
+    public class ChequeController : ApiController
+    {
+        public HttpResponseMessage Post([FromBody] Cheque cheque)
+        {
+            if (cheque.agregarCuentaCheque("Insertar"))
+            {
+                var message = string.Format("Se guardó con éxito la cuenta y el cheque");
+                return Request.CreateResponse(HttpStatusCode.OK, message);
+            }
+            else
+            {
+                var message = string.Format("No se guardó la cuenta y el cheque.");
+                return Request.CreateResponse(HttpStatusCode.NotAcceptable, message);
+            }
+        }
+
+        public HttpResponseMessage Put(string id, [FromBody] Cheque cheque)
+        {
+            int resultado = cheque.modificarCheque("Actualizar");
+            if (resultado == -1)
+            {
+                var message = string.Format("Numero de cheque invalido");
+                return Request.CreateResponse(HttpStatusCode.OK, message);
+            }
+            else if (resultado==-2)
+            {
+                var message = string.Format("Numero de cuenta invalido");
+                return Request.CreateResponse(HttpStatusCode.OK, message);
+            }
+            else if (resultado == -3)
+            {
+                var message = string.Format("Fondos insuficientes");
+                return Request.CreateResponse(HttpStatusCode.OK, message);
+            }
+            else 
+            {
+                var message = string.Format("Cheque aceptado");
+                return Request.CreateResponse(HttpStatusCode.OK, message);
+
+            }
+
+
+        }
+    }
+}
